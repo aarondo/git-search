c120cabb0af76ff7ecf9840d7de6cd39be425354
Aaron Lumsden <dev@citu.co.uk>
2020-01-09 15:53:04 +0000

    - Added dist folder so that the website can previewed

---
+++ dist/precache-manifest.4d4a5411f4f1542b24ae2044e32a4605.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a593e2ec64af3d43cd22",
+    "url": "/css/app.ff645c23.css"
+  },
+  {
+    "revision": "a593e2ec64af3d43cd22",
+    "url": "/css/app.ff645c23.css.map"
+  },
+  {
+    "revision": "ddae4e94fce495bb05f2",
+    "url": "/css/chunk-vendors.b0169879.css"
+  },
+  {
+    "revision": "ddae4e94fce495bb05f2",
+    "url": "/css/chunk-vendors.b0169879.css.map"
+  },
+  {
+    "revision": "f2e66164e5cc8ea136ade3506df08b5a",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "f2e66164e5cc8ea136ade3506df08b5a",
+    "url": "/img/icons/favicon.ico"
+  },
+  {
+    "revision": "181aafab85beb199026ad4818890d3d3",
+    "url": "/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "d6f46cd0c73a63d46023593ee3078932",
+    "url": "/img/logo.svg"
+  },
+  {
+    "revision": "a593e2ec64af3d43cd22",
+    "url": "/js/app.4ea903a6.js"
+  },
+  {
+    "revision": "a593e2ec64af3d43cd22",
+    "url": "/js/app.4ea903a6.js.map"
+  },
+  {
+    "revision": "ddae4e94fce495bb05f2",
+    "url": "/js/chunk-vendors.68993abe.js"
+  },
+  {
+    "revision": "ddae4e94fce495bb05f2",
+    "url": "/js/chunk-vendors.68993abe.js.map"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
